08945c1a9b678ecc01fa973dbde3ab4b3d7e17cd
Vasile Grosu <vasilegrosu@Vasiles-MacBook-Pro.local>
2022-09-07 15:10:39 +0300

    added register form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,22 +3,27 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
-
-
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgxMaskModule.forRoot()
   ],
   exports: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
 
 })
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { min } from 'rxjs';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+
+
+export class RegisterComponent {
+
+  name = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3)
+  ])
+  email = new FormControl('', [
+    Validators.email,
+    Validators.required
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(16),
+    Validators.max(99)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{6,})\S$/)
+  ])
+  confirm_password = new FormControl('', [
+    Validators.required,
+
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
+
+  registerForm = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirm_password: this.confirm_password,
+    phoneNumber: this.phoneNumber
+  })
+
+  register() {
+
+  }
+
+}
