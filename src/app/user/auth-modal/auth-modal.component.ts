e4c4e86e4731d51cc8eaf2c785b70f3ab9e50d1d
Vasile <maxdsl98@gmail.com>
2022-10-06 18:08:31 +0300

    added translations

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
+import { TranslationsService } from 'src/app/services/translations.service';
 
 @Component({
   selector: 'app-auth-modal',
@@ -8,7 +9,7 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class AuthModalComponent implements OnInit, OnDestroy {
 
-  constructor(public modal: ModalService) { }
+  constructor(public modal: ModalService, public translations: TranslationsService) { }
 
   ngOnInit(): void {
     this.modal.register('auth')
